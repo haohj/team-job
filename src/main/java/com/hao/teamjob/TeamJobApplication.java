99a5cdcbbad4619ec9e2bc24e0a12424cc6313a2
haohj <haohj94@163.com>
2019-06-24 09:59:47 +0800

    first commit

---
+++ src/main/java/com/hao/teamjob/TeamJobApplication.java
@@ -0,0 +1,13 @@
+package com.hao.teamjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamJobApplication.class, args);
+    }
+
+}
