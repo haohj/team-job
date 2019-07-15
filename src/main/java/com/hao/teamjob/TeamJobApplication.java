1d2dd858fa92b34c2b42e6132e86b56654610d7b
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-15 16:08:15 +0800

    启用swagger

--- src/main/java/com/hao/teamjob/TeamJobApplication.java
+++ src/main/java/com/hao/teamjob/TeamJobApplication.java
@@ -1,9 +1,11 @@
 package com.hao.teamjob;
 
+import com.battcn.swagger.annotation.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2Doc
 public class TeamJobApplication {
 
     public static void main(String[] args) {
