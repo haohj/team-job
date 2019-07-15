9a0848407307ab6e93c685c4d145fd2b9368c596
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-15 16:22:30 +0800

    修改项目为使用tomcat启动

---
+++ src/main/java/com/hao/teamjob/SpringBootStartApplication.java
@@ -0,0 +1,11 @@
+package com.hao.teamjob;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TeamJobApplication.class);
+    }
+}
