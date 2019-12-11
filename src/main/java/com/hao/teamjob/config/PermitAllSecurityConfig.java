53c86a593298064ff1d2c1ef63465643f5e64560
郝红剑 <hongjian.hao@zjtzsw.com>
2019-12-11 15:32:22 +0800

    docs: 添加注释

--- src/main/java/com/hao/teamjob/config/PermitAllSecurityConfig.java
+++ src/main/java/com/hao/teamjob/config/PermitAllSecurityConfig.java
@@ -1,8 +1,36 @@
 package com.hao.teamjob.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+
 /**
  * @author haohj
  * @date 2019-12-11 14:44
  */
-public class PermitAllSecurityConfig {
+@Component("permitAllSecurityConfig")
+public class PermitAllSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    @Autowired
+    private Filter uriFormatFilter;
+
+    /**
+     * 注册自定义的url格式化过滤器
+     *
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean setFilter() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(uriFormatFilter);
+        filterBean.setName("uriFormatFilter");
+        filterBean.addUrlPatterns("/*");
+        filterBean.setOrder(-10000);
+        return filterBean;
+    }
 }
--- src/main/java/com/hao/teamjob/filter/UriFormatFilter.java
+++ src/main/java/com/hao/teamjob/filter/UriFormatFilter.java
@@ -1,8 +1,42 @@
 package com.hao.teamjob.filter;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
+ * url格式化过滤器
+ *
  * @author haohj
  * @date 2019-12-11 14:47
  */
-public class UriFormatFilter {
+@Component("uriFormatFilter")
+public class UriFormatFilter extends OncePerRequestFilter {
+    /**
+     * 格式化url，将url中的'//'替换为'/'
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String uri = httpServletRequest.getRequestURI();
+        String newUri = uri.replace("//", "/");
+        httpServletRequest = new HttpServletRequestWrapper(httpServletRequest) {
+            @Override
+            public String getRequestURI() {
+                return newUri;
+            }
+        };
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
 }
