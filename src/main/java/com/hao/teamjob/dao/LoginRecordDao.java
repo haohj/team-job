0fb11e22459c7cd2243fd209fe572ee23e766500
郝红剑 <hongjian.hao@zjtzsw.com>
2020-03-12 14:48:45 +0800

    feat: 首页和用户信息

--- src/main/java/com/hao/teamjob/dao/LoginRecordDao.java
+++ src/main/java/com/hao/teamjob/dao/LoginRecordDao.java
@@ -1,4 +1,14 @@
 package com.hao.teamjob.dao;
 
+import com.hao.teamjob.domain.LoginRecord;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+
+@Mapper
 public interface LoginRecordDao {
+    void addRecord(LoginRecord loginRecord);
+
+    void updateRecord(@Param("logoutTime") Date logoutTime, @Param("id") int id);
 }
