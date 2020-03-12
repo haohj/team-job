4236159adf405216020422dfda55254319b735a1
郝红剑 <hongjian.hao@zjtzsw.com>
2020-03-12 16:15:08 +0800

    feat: 项目模板管理功能开发

--- src/main/java/com/hao/teamjob/dao/ProjectTemplateDao.java
+++ src/main/java/com/hao/teamjob/dao/ProjectTemplateDao.java
@@ -1,4 +1,17 @@
 package com.hao.teamjob.dao;
 
+import com.hao.teamjob.domain.ProjectTemplate;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface ProjectTemplateDao {
+    void add(ProjectTemplate projectTemplate);
+
+    void update(ProjectTemplate projectTemplate);
+
+    List<ProjectTemplate> getAllProjectte();
+
+    ProjectTemplate getProjectById(String id);
 }
