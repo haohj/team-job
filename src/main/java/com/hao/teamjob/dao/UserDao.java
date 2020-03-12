bd9e958ae91adfc9e8ab492f1030bae84bdf8b7a
郝红剑 <hongjian.hao@zjtzsw.com>
2020-03-12 16:59:56 +0800

    feat: 统计git代码提交量和首页修改

--- src/main/java/com/hao/teamjob/dao/UserDao.java
+++ src/main/java/com/hao/teamjob/dao/UserDao.java
@@ -1,4 +1,29 @@
 package com.hao.teamjob.dao;
 
+import com.hao.teamjob.domain.User;
+import com.hao.teamjob.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
 public interface UserDao {
+    void addUser(User user);
+
+    void update(User user);
+
+    void updatePassword(User user);
+
+    List<UserDto> selectAllUser();
+
+    UserDto selectUserById(@Param("id") String id);
+
+    UserDto selectUserByRepositoryUsername(String repositoryUsername);
+
+    UserDto selectUserByEmail(String email);
+
+    List<UserDto> selectUserByIds(@Param("ids") List<String> ids);
+
+    List<UserDto> selectUserWithoutIds(@Param("ids") List<String> ids);
 }
