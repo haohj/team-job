40d5cbb9f576f6f904a4f9ee75dcf5698c4b885d
郝红剑 <hongjian.hao@zjtzsw.com>
2020-03-12 14:06:57 +0800

    feat: 统计git提交代码量

--- src/main/java/com/hao/teamjob/config/GitContentsConfig.java
+++ src/main/java/com/hao/teamjob/config/GitContentsConfig.java
@@ -1,8 +1,24 @@
 package com.hao.teamjob.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 /**
+ * 统计git提交代码量
+ *
  * @author haohj
  * @date 2019-12-11 14:43
  */
+@Component
+@ConfigurationProperties(prefix = "git")
 public class GitContentsConfig {
+    private String basePath;
+
+    public String getBasePath() {
+        return basePath;
+    }
+
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
 }
