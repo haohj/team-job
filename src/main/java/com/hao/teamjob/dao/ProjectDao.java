68b2999a7a0a0afbc81e68effe8d5031d7879be7
郝红剑 <hongjian.hao@zjtzsw.com>
2020-03-12 16:07:51 +0800

    feat: 应用创建查看修改删除

--- src/main/java/com/hao/teamjob/dao/ProjectDao.java
+++ src/main/java/com/hao/teamjob/dao/ProjectDao.java
@@ -1,10 +1,12 @@
 package com.hao.teamjob.dao;
 
 import com.hao.teamjob.domain.Project;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface ProjectDao {
     void addProject(Project project);
 
